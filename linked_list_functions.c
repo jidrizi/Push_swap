41ff58913c44cec31576bbad544426d245d99a7c
jidrizi <idrizijuxhin@gmail.com>
2024-07-19 15:56:09 +0200

    added id to chain links

--- linked_list_functions.c
+++ linked_list_functions.c
@@ -6,13 +6,13 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 19:07:15 by jidrizi           #+#    #+#             */
-/*   Updated: 2024/07/18 19:18:25 by jidrizi          ###   ########.fr       */
+/*   Updated: 2024/07/19 15:52:17 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_chain_link	*make_new_chain_link(int data)
+static t_chain_link	*make_new_chain_link(int data, int link_id)
 {
 	t_chain_link	*new_chain_link;
 
@@ -20,6 +20,7 @@ static t_chain_link	*make_new_chain_link(int data)
 	if (new_chain_link == NULL)
 		return (NULL);
 	new_chain_link->data = data;
+	new_chain_link->id = link_id;
 	return (new_chain_link);
 }
 
@@ -32,21 +33,28 @@ static void	link_chainlinks(t_chain_link *current_link, t_chain_link *next_link)
 int	make_linked_list(int *integer_array, int argc)
 {
 	int				current_int;
+	int				link_id;
 	t_chain_link	*link;
 	t_chain_link	*temp_link;
-	
-	link = make_new_chain_link(integer_array[current_int]);
+
+	current_int = 0;
+	link_id = 0;
+	link = make_new_chain_link(integer_array[current_int], link_id);
+	ft_printf("%d : %d\n", link->data, link->id);
 	if (link == NULL)
 		return (EXIT_FAILURE);
-	current_int = 1;
+	current_int++;
 	while (current_int < argc - 1)
 	{
-		temp_link = make_new_chain_link(integer_array[current_int]);
+		link_id++;
+		temp_link = make_new_chain_link(integer_array[current_int], link_id);
+		ft_printf("%d : %d\n", temp_link->data, temp_link->id);
 		if (temp_link == NULL)
 			return (free(link), EXIT_FAILURE);
 		link_chainlinks(link, temp_link);
 		link = temp_link;
 		current_int ++;
 	}
+	
 	return (EXIT_SUCCESS);
 }
