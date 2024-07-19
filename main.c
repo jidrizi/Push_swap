a2bce2db03f3fe1be1908e0b329c5ee73e22dbbf
jidrizi <idrizijuxhin@gmail.com>
2024-07-19 15:55:50 +0200

    changed the main so now if you only give the executable and 1 number it returns 0

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:21:08 by jidrizi           #+#    #+#             */
-/*   Updated: 2024/07/17 20:27:57 by jidrizi          ###   ########.fr       */
+/*   Updated: 2024/07/19 15:54:31 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char *argv[])
 	t_chain_link	*linked_list;
 	
 	linked_list = (t_chain_link *)ft_calloc(1, sizeof(t_chain_link));
-	if (argc == 1)
+	if (argc == 1 || argc == 2)
 		return (EXIT_SUCCESS);
 	if (total_arguments_parsing(argc, argv) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
