ca5a134c66c8b8176bbe68fe887cd04639ae51fd
jidrizi <idrizijuxhin@gmail.com>
2024-07-19 19:02:53 +0200

    trying to do pa move correctly

--- pushes.c
+++ pushes.c
@@ -6,20 +6,19 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:26:12 by jidrizi           #+#    #+#             */
-/*   Updated: 2024/07/19 17:30:19 by jidrizi          ###   ########.fr       */
+/*   Updated: 2024/07/19 19:02:09 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void pa(t_chain_link *a, t_chain_link *b)
-// {
-// 	t_chain_link	*tmp;
-
-// 	if (b->next == NULL)
-// 		return ;
-// 	tmp = b->next;
-// 	b->next = b->next->next;
-// 	tmp->next = a->next;
-// 	a->next = tmp;
-// }
+void pa(t_chain_link *a, t_chain_link *b, int argc)
+{
+	(void)argc;
+	if (b == NULL)
+		return ;
+	b->data = a->data;
+	a->data = a->next->data;
+	// rearange_list_after_move(a, argc);
+	ft_printf("pa\n");
+}
